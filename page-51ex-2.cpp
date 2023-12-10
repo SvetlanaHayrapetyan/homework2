5740f88ecdca306fcbc9e64f6fb993746d4a2ea4
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2023-12-10 14:52:32 +0400

    page-51ex-2.cpp

---
+++ page-51ex-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    
+    int K, N;
+
+    std::cin >> K >> N;
+
+      for (int i = 0; i < N; ++i) {
+        std::cout << K << std::endl;
+    }
+
+    return 0;
+}
