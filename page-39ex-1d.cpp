7f7244316af255cd3e36afe184f82707a7eb33fa
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2023-12-10 12:13:55 +0400

    page-39ex-1d.cpp

---
+++ page-39ex-1d.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    int a;
+    int b;
+    
+    std::cin >> a;
+    std::cin >> b;
+ 
+    if(( a % 2 != 0 && b % 2 != 0) || (a % 2 == 0 && b % 2 == 0))
+    std::cout << "YES" << std::endl;
+else 
+     std::cout << "NO" << std::endl;
+
+    
+    return 0;
+}
