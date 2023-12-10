b4493e596d108f79c3fccefe4fad1ac4e5f712ff
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2023-12-10 14:33:43 +0400

    page-43ex-13b.cpp

---
+++ page-43ex-13b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    
+    double x, y, r;
+    std::cin >> x >> y >> r;
+
+    if (x * x + y * y < r * r) {
+        std::cout << "YES" << std::endl;
+    } else {
+        std::cout << "NO" << std::endl;
+    }
+
+    return 0;
+}
