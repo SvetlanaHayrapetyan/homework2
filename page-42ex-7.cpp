0a0721478f8960e9781c271f626fcf7ad99f442d
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2023-12-10 13:48:00 +0400

    page-42ex-7.cpp

--- page-42ex-7.cpp
+++ page-42ex-7.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+int main()
+{
+    unsigned int x, y, x1, y1, x2, y2;
+    
+    std::cin >> x >> y >> x1 >> y1 >> x2 >> y2;
+    
+     if (x >= x1 && x <= x2 && y <= y1 && y >= y2) {
+         std::cout << "YES" << std::endl;
+        
+    } else {
+        std::cout << "NO" << std::endl;
+    }
+
+    return 0;
+}
