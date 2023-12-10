de0876b48e62c8941797c8312e42890717ea2adf
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2023-12-10 13:22:44 +0400

    page-42ex5b.cpp

---
+++ page-42ex5b.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    
+    std::cin >> a;
+    
+    if(a<100 || a>999){
+        
+        std::cout << " " << std::endl;
+        
+    }
+    
+    else{
+       int number1 = a/100 ;
+       int number2 = (a/10) % 10 ;
+       int number3 = a % 110 ;
+       
+       
+       if (number1 <= number2 && number2 <= number3 && number1 < number3 )   {
+          std::cout << "YES" << std::endl;
+        }
+       else {
+          std::cout << "NO" << std::endl;
+        }  
+    }
+    return 0;
+}
